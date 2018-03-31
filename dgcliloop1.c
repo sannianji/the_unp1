644cab1a87a3691cee8ee8584100378d18c8fe93
sansansan <sannianjisansan@gmail.com>
2018-03-31 17:36:10 +0800

    udp

--- dgcliloop1.c
+++ dgcliloop1.c
@@ -1,6 +1,6 @@
 #include "unp.h"
 
-#define NDG 2000
+#define NDG 20000
 #define DGLEN 1400
 
 void dg_cli(FILE *fp,int sockfd,const SA *pservaddr,socklen_t servlen)
@@ -11,4 +11,9 @@ void dg_cli(FILE *fp,int sockfd,const SA *pservaddr,socklen_t servlen)
 	for(i=0;i<NDG;i++)
 		if(sendto(sockfd,sendline,DGLEN,0,pservaddr,servlen)!=DGLEN)
 			err_sys("sendto");
+		else
+		{
+			printf("finished\n");
+			fflush(stdout);
+		}
 }

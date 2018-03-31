e4775d368208213ec039c801664f936d4c8ea77d
sannianji <sannianjisansan@gmail.com>
2018-03-31 17:56:45 +0800

    dgecholoop2 SO_RCVBUF

---
+++ dgecholoop2.c
@@ -0,0 +1,34 @@
+#include "unp.h"
+
+static void recvfrom_init(int);
+static int count;
+
+void dg_echo(int sockfd,SA *pcliaddr,socklen_t clilen)
+{
+	int n;
+	socklen_t len;
+	char mesg[MAXLINE];
+
+	if(signal(SIGINT,recvfrom_init)<0)
+		err_sys("signal");
+
+	n=220*1024;
+	
+	if(setsockopt(sockfd,SOL_SOCKET,SO_RCVBUF,&n,sizeof(n))<0)
+		err_sys("setsockopt");
+
+	for(;;)
+	{
+		len=clilen;
+		if(recvfrom(sockfd,mesg,MAXLINE,0,pcliaddr,&len)<0)
+			err_sys("recvfrom");
+		count++;
+		printf("recieved %d\n",count);
+		fflush(stdout);
+	}
+}
+static void recvfrom_init(int signo)
+{
+	printf("\nreceived %d datagrams\n",count);
+	exit(0);
+}
